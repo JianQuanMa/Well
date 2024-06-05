b65cd11c2465d52801d28991cb8d73ab4e8223cf
Jian Ma <jianquanma@gmail.com>
2024-06-05 11:26:27 -0400

    Initial Commit

---
+++ Well/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Well
+//
+//  Created by Jian Ma on 6/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
